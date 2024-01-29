f2d27f1559093d21500e66c25d91124081cdf03d
wysha <wyshazhisishen@yeah.net>
2024-01-30 01:22:55 +0800

    1.0.3

---
+++ src/cn/com/wysha/debate_tournament/data/Config.java
@@ -0,0 +1,51 @@
+package cn.com.wysha.debate_tournament.data;
+
+import cn.com.wysha.debate_tournament.main.Bout;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+
+/**
+ * @param prosName 正方名
+ * @param consName 反方名
+ * @author wysha
+ */
+public record Config(
+        String name,
+        String prosName,
+        String consName,
+        String thesis,
+        LinkedList<Bout> bouts
+) implements Serializable {
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public String prosName() {
+        return prosName;
+    }
+
+    @Override
+    public String consName() {
+        return consName;
+    }
+
+    @Override
+    public String thesis() {
+        return thesis;
+    }
+
+    @Override
+    public LinkedList<Bout> bouts() {
+        return bouts;
+    }
+
+    public static Config config;
+
+    @Override
+    public String toString() {
+        return name+"           论题:"+thesis+"           包含回合数:"+bouts.size();
+    }
+}
---
+++ src/cn/com/wysha/debate_tournament/set/settings/AbstractSettings.java
@@ -0,0 +1,15 @@
+
+package cn.com.wysha.debate_tournament.set.settings;
+
+import cn.com.wysha.debate_tournament.set.AbstractSetSubpages;
+
+import javax.swing.*;
+
+/**
+ * @author wysha
+ */
+public abstract class AbstractSettings extends AbstractSetSubpages {
+    protected AbstractSettings(String name, JDialog jDialog) {
+        super(name, jDialog);
+    }
+}
